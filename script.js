bec493dc9ef08731a2f4d59bdcd99c7d0a24f6a4
Anthony Mendoza <anthony.mendoza310@gmail.com>
2020-03-10 19:04:49 -0700

    wrote gameboard() and cells()

--- script.js
+++ script.js
@@ -1,31 +1,64 @@
-const player = function() {
-    let symbol;
-    let name;
-    const updateInfo = str => name = str;
-    const submitMove = function() {
+const players = function (name, symbol) {
+    const getName = () => name;
+    const submitMove = function (location) {
+        return location.textContent = symbol;
     }
-    return {submitMove, updateInfo}
+    return { getName, submitMove }
 }
 
-const spaces = () => { 
-    let state
-    let id;
-    const render = () => {
-    };
+const cells = (id) => {
+    let state;
+    const getId = () => id;
+    const getState = () => state;
     const changeState = symbol => state = symbol;
-    return {render, changeState};
+    return { getId, getState, changeState };
 }
 
 const gameBoard = (() => {
     const board = [];
-    const render = () => {
-        for(let i = 0;i<board.length;i++){
-            board[i]
+    const cellElement = document.createElement("DIV");
+    const gameContainer = document.getElementById("gameContainer")
+    const fillBoard = () => {
+        for (let i = 0; i < 9; i++) {
+            board.push(cells(i));
         }
     }
+    const renderCell = () => {
+        for (let i = 0; i < board.length; i++) {
+            gameContainer.appendChild(cellElement.cloneNode());
+            gameContainer.lastElementChild.className = "cells"
+            gameContainer.lastElementChild.id = i
+        }
+    };
+    const applyEvent = () => {
+        let cellElements = document.querySelectorAll(".cells")
+        for (let i = 0; i < cellElements.length; i++) {
+            cellElements[i].addEventListener("click", (e) => currentPlayer.submitMove(e));
+            // cellElements[i].addEventListener("click", (e) => console.log(gameProcess.getTest()));
+        }
+    }
+    const getBoard = () => board
+    return { fillBoard, renderCell, applyEvent, getBoard }
 })();
 
-const Process = (() => {
+const gameProcess = (() => {
+    // let test = "test"
+    const init = () => {
+        gameBoard.fillBoard();
+        gameBoard.renderCell();
+        gameBoard.applyEvent();
+    }
+    // const changeTest = (str) => test = str;
+    // const getTest = () => test
+    let turn = 0;
+    const trackTurn = () => {
+        turn++
+        if (turn%2>1) {
+            currentPlayer = playerList[0]
+        } else {
+            currentPlayer = playerList[1]
+        }
+    }
     const receiveMove = () => {
 
     }
@@ -35,5 +68,5 @@ const Process = (() => {
     const announceResults = () => {
 
     }
-    return{receiveMove,checkMove,announceResults}
+    return { changeTest, getTest, init, trackTurn, receiveMove, checkMove, announceResults }
 })();
